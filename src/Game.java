299146682755b93c6b3859619dcf659568ae279a
Datureli <soleris1@gmail.com>
2024-04-06 19:45:46 +0200

    fix location visits

--- src/Game.java
+++ src/Game.java
@@ -1,69 +1,45 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Game {
-    private List<Level> levels = new ArrayList<>();
-    private int currentLevelIndex = 0;
-    private Scanner scanner = new Scanner(System.in);
+    private List<Locations.Location> locations;
+    private Locations.Location currentLocation;
+    private Scanner scanner;
 
     public Game() {
-        String[] levelNames = Locations.levelNames;
-        String[][] levelOptions = Locations.levelOptions;
-
-        for (int i = 0; i < levelNames.length; i++) {
-            List<String> locations = new ArrayList<>();
-            locations.add(levelNames[i]);
-            List<String[]> options = new ArrayList<>();
-            options.add(levelOptions[i]);
-            levels.add(new Level("Poziom " + (i + 1), locations, options));
-        }
+        locations = Locations.createLocations();
+        currentLocation = locations.get(0); // Ustawienie początkowej lokalizacji na pierwszą lokalizację z listy
+        scanner = new Scanner(System.in);
     }
 
     public void startGame() {
         String playerName = PlayerUtils.welcomePlayer();
 
-        while (currentLevelIndex < levels.size()) {
-            Level currentLevel = levels.get(currentLevelIndex);
-            System.out.println("Jesteś na " + currentLevel.getName());
+        while (currentLocation != null) {
+            System.out.println("You are at " + currentLocation.name);
 
-            List<String> locations = currentLevel.getLocations();
-            List<String[]> options = currentLevel.getOptions();
+            List<Locations.Option> options = currentLocation.options;
 
             for (int i = 0; i < options.size(); i++) {
-                String locationName = locations.get(i);
-                System.out.println((i + 1) + ". " + locationName);
+                Locations.Option option = options.get(i);
+                System.out.println((i + 1) + ". " + option.optionName);
             }
 
-            System.out.println("Wybierz opcję (1-" + options.size() + "): ");
+            System.out.println("Choose an option (1-" + options.size() + "): ");
             int choice = scanner.nextInt();
 
             if (choice >= 1 && choice <= options.size()) {
-                String[] chosenOptions = options.get(choice - 1);
-                System.out.println("Jesteś teraz w lokalizacji: " + chosenOptions[0]); // Wyświetl aktualną lokalizację
-
-                if (chosenOptions.length > 1) {
-                    System.out.println("Dostępne opcje:");
-
-                    for (int i = 0; i < chosenOptions.length; i++) {
-                        System.out.println((i + 1) + ". " + chosenOptions[i]);
+                // Aktualizacja bieżącej lokalizacji na podstawie wyboru gracza
+                Locations.Option selectedOption = options.get(choice - 1);
+                for (Locations.Location location : locations) {
+                    if (selectedOption.optionValue.equals(location.name)) {
+                        currentLocation = location;
+                        break;
                     }
-
-                    System.out.println("Wybierz opcję (1-" + chosenOptions.length + "): ");
-                    int optionChoice = scanner.nextInt();
-
-                    if (optionChoice >= 1 && optionChoice <= chosenOptions.length) {
-                        // Zaktualizuj aktualny poziom na podstawie wyboru
-                        currentLevelIndex++;
-                    }
-                } else {
-                    System.out.println("Gratulacje, ukończyłeś poziom " + currentLevel.getName() + "!");
-                    currentLevelIndex++; // Przejdź do następnego poziomu
                 }
             } else {
-                System.out.println("Nieprawidłowy wybór. Wybierz numer od 1 do " + options.size());
+                System.out.println("Invalid choice. Please choose a number from 1 to " + options.size());
             }
         }
-        System.out.println("Gratulacje, ukończyłeś wszystkie poziomy gry!");
     }
 }
--- src/Locations.java
+++ src/Locations.java
@@ -1,10 +1,62 @@
-public class Locations {
-    public static String[] levelNames = {"Skrzyżowanie", "Las", "Bagna", "Wioska"};
+import java.util.Arrays;
+import java.util.List;
 
-    public static String[][] levelOptions = {
-            {"Idz na bagna", "Idz do Lasu"},
-            {"Idz do Lasu", "Wroc na Skrzyżowanie", "Idz do Rzeki", "Idz na Bagna"},
-            {"Idz na Las", "Wroc na Skrzyżowanie"},
-            {"Idz do Lasu", "Wroc na Bagna"}
-    };
+public class Locations {
+    public static class Location {
+        String name;
+        List<Option> options;
+
+        public Location(String name, List<Option> options) {
+            this.name = name;
+            this.options = options;
+        }
+
+        @Override
+        public String toString() {
+            return "LocationName: " + name + ", LocationOptions: " + options.toString();
+        }
+    }
+
+    public static class Option {
+        String optionName;
+        String optionValue;
+
+        public Option(String optionName, String optionValue) {
+            this.optionName = optionName;
+            this.optionValue = optionValue;
+        }
+
+        @Override
+        public String toString() {
+            return "OptionName: " + optionName + ", OptionValue: " + optionValue;
+        }
+    }
+
+    public static List<Location> createLocations() {
+        return Arrays.asList(
+                new Location("Crossroad", Arrays.asList(
+                        new Option("Go to the swamps", "Swamps"),
+                        new Option("Go to the forest", "Forest")
+                )),
+                new Location("Swamps", Arrays.asList(
+                        new Option("Go to the forest", "Forest"),
+                        new Option("Return to the crossroad", "Crossroad")
+                )),
+                new Location("Forest", Arrays.asList(
+                        new Option("Return to the crossroad", "Crossroad"),
+                        new Option("Go to the river", "River"),
+                        new Option("Go to the swamps", "Swamp")
+                )),
+                new Location("River", Arrays.asList(
+                        new Option("Go to the forest", "Forest"),
+                        new Option("Return to the crossroad", "Crossroad")
+                ))
+        );
+    }
+
+    public static void main(String[] args) {
+        for (Location location : createLocations()) {
+            System.out.println(location);
+        }
+    }
 }
