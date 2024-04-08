d66b7e5f9fe4ba228f382f8aadf698cff1981477
Datureli <soleris1@gmail.com>
2024-04-08 17:18:23 +0200

    Add messageAppear on location

--- src/Locations.java
+++ src/Locations.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -5,15 +6,23 @@ public class Locations {
     public static class Location {
         String name;
         List<Option> options;
+        Message message;
 
-        public Location(String name, List<Option> options) {
+        public Location(String name, List<Option> options, Message message) {
             this.name = name;
             this.options = options;
+            this.message = message;
         }
 
         @Override
         public String toString() {
-            return "LocationName: " + name + ", LocationOptions: " + options.toString();
+            StringBuilder sb = new StringBuilder();
+            sb.append("LocationName: ").append(name);
+            sb.append(", LocationOptions: ").append(options);
+            if (message != null) {
+                sb.append(", LocationMessage: ").append(message.toMessage());
+            }
+            return sb.toString();
         }
     }
 
@@ -32,25 +41,38 @@ public class Locations {
         }
     }
 
+    public static class Message {
+        String appearMessage;
+
+        public Message(String appearMessage) {
+            this.appearMessage = appearMessage;
+        }
+
+        public String toMessage() {
+            return appearMessage;
+        }
+    }
+
     public static List<Location> createLocations() {
         return Arrays.asList(
                 new Location("Crossroad", Arrays.asList(
                         new Option("Go to the swamps", "Swamps"),
                         new Option("Go to the forest", "Forest")
-                )),
+                ), null),
                 new Location("Swamps", Arrays.asList(
+                        new Option("Check swamps", "SwampCheck"),
                         new Option("Go to the forest", "Forest"),
                         new Option("Return to the crossroad", "Crossroad")
-                )),
+                ), new Message("s")),
                 new Location("Forest", Arrays.asList(
                         new Option("Return to the crossroad", "Crossroad"),
                         new Option("Go to the river", "River"),
                         new Option("Go to the swamps", "Swamp")
-                )),
+                ), null),
                 new Location("River", Arrays.asList(
                         new Option("Go to the forest", "Forest"),
                         new Option("Return to the crossroad", "Crossroad")
-                ))
+                ), null)
         );
     }
 
